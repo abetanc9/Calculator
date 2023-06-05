3562a6f1f0ea010e9d07340c6c51fe32db0abf26
Santiago <santibeta133@gmail.com>
2023-06-05 02:16:37 -0500

    calculator is here

---
+++ index.js
@@ -0,0 +1,17 @@
+function numeros(valor) {
+    document.getElementById('resultado').value += valor;
+}
+
+function operacion() {
+    let opera = document.getElementById('resultado').value;
+    if( opera == 0){
+        document.getElementById('resultado').value = "Sin operacion";
+    } else{
+        document.getElementById('resultado').value = eval(opera);
+    }    
+}
+
+function resetear() {
+    document.getElementById('resultado').value = " ";
+    
+}
